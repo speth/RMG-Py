289736bc7eba6815fc974ce463bf9a33a83f976d
alongd <alongd@mit.edu>
2018-06-15 10:37:25 -0400

    Modified the C2H4 example to generate a .yml file
    Simply by providing the structure

--- examples/cantherm/species/C2H4/input.py
+++ examples/cantherm/species/C2H4/input.py
@@ -4,8 +4,12 @@
 modelChemistry = "CBS-QB3"
 useHinderedRotors = True
 useBondCorrections = False
+author = 'I.B. Modeling'
 
-species('C2H4', 'ethene.py')
+species('C2H4', 'ethene.py',
+        structure=SMILES('C=C'),
+        )
 
 statmech('C2H4')
+
 thermo('C2H4', 'NASA')
